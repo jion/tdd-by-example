175623c66331f52d2fc83e51782d94d65e5b26bf
Manuel Schnidrig <manuel.schnidrig@ordergroove.com>
2019-01-05 17:37:05 -0300

    Chapter 19 - Set the table

--- 2-the-xUnit-example/xunit.py
+++ 2-the-xUnit-example/xunit.py
@@ -2,25 +2,38 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)
 
+    def setUp(self):
+        self.wasRun = False
+        self.wasSetup = True
+
     def testMethod(self):
-        self.wasRun = 1
+        self.wasRun = True
 
 
 class TestCaseTest(TestCase):
-    def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+    def setUp(self):
+        self.test = WasRun("testMethod")
 
+    def testSetup(self):
+        self.test.run()
+        assert(self.test.wasSetup)
+
+    def testRunning(self):
+        self.test.run()
+        assert(self.test.wasRun)
+
+TestCaseTest('testSetup').run()
 TestCaseTest('testRunning').run()
