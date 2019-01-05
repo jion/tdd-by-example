359b9c5c212de618872cb4045bb7396c9363da36
Manuel Schnidrig <manuel.schnidrig@ordergroove.com>
2019-01-05 19:00:09 -0300

    Chapter 21 - Counting

---
+++ 2-the-xUnit-example/TODO.md
@@ -0,0 +1,10 @@
+# List if things pending to do
+
+* ~~Invoke test method~~
+* ~~Invoke SetUp first~~
+* ~~Invoke tearDown afterward~~
+* Invoke tearDown even if the test method fails
+* Run multiple tests
+* ~~Report collected results~~
+* ~~Log string in WasRun~~
+* Report failed tests
--- 2-the-xUnit-example/xunit.py
+++ 2-the-xUnit-example/xunit.py
@@ -9,10 +9,26 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.test_started()
+
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+
+        return result
+
+
+class TestResult:
+    def __init__(self):
+        self.run_count = 0
+
+    def test_started(self):
+        self.run_count += 1
+
+    def summary(self):
+        return "{} run, 0 failed".format(self.run_count)
 
 
 class WasRun(TestCase):
@@ -30,6 +46,9 @@ class WasRun(TestCase):
         self.log += "testMethod "
         self.wasRun = True
 
+    def testBrokenMethod(self):
+        raise Exception
+
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
@@ -37,4 +56,17 @@ class TestCaseTest(TestCase):
         self.test.run()
         assert("setUp testMethod tearDown " == self.test.log)
 
+    def testResult(self):
+        self.test = WasRun("testMethod")
+        result = self.test.run()
+        assert("1 run, 0 failed" == result.summary())
+
+    def testFailedResult(self):
+        self.test = WasRun("testBrokenMethod")
+        result = self.test.run()
+        assert("1 run, 1 failed" == result.summary())
+
+
 TestCaseTest('testTemplateMethod').run()
+TestCaseTest('testResult').run()
+# TestCaseTest('testFailedResult').run()
